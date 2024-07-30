3fa67f17dd7769c47ac36ed95d4313b3f8ed895f
Mustafa <mustafababar2003@gmail.com>
2024-07-30 13:59:18 +0500

    Upload website (work-in-progress) to Git

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function showMenu() {
+    let elem = document.getElementById('main-menu');
+    // elem.style.display = "block";
+    if (elem.style.display == "block") {
+        elem.style.display = "none";
+    }
+    else {
+        elem.style.display = "block";
+    }
+}
